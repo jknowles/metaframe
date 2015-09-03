76d6e80515fdedc0c0c0b5e63347cf22282e3610
Jared Knowles <jknowles@gmail.com>
2015-09-03 10:09:40 -0500

    rough codebook function up

--- R/codebook.R
+++ R/codebook.R
@@ -1,5 +1,30 @@
-# Make a codebook
+##' Generic function to build a codebook from an R object
+##'
+##' Build an HTML codebook from an R object that includes metadata about the 
+##' data stored in the object and its creation. For some object types it will 
+##' include auto-generated summary information about the data in the fields. 
+##' @usage codebook(data, file, replace, render)
+##' @param data a data object to construct a codebook for
+##' @param file a character string indicating where the file should be saved, if 
+##' NULL default is `codebook`
+##' @param replace logical, should an existing codebook file be overwritten, default 
+##' is FALSE
+##' @param render logical, should the codebook be opened after it is written? default 
+##' is FALSE
+##' @return An HTML file with codebook information
+##' @export codebook
+##' @rdname codebook
+##' @author Carl Frederick
+codebook <- function(data, file=NULL, replace=FALSE, render=FALSE){
+  UseMethod("codebook")
+}
 
+
+
+#' @title Build a codebook from a meta.data object
+#' @rdname codebook
+#' @method codebook meta.data
+#' @export
 codebook.meta.data <- function(object, file=NULL, replace=FALSE, render=FALSE) {
   if (!inherits(object, "meta.data")) {
     stop("not a meta.data object")
@@ -36,10 +61,10 @@ codebook.meta.data <- function(object, file=NULL, replace=FALSE, render=FALSE) {
   
   #Cycle through (remember the two trailing spaces!)
   for (i in 1:nvars) {
-    idx <-  names(object@description)==varnames[i]
+    idx <-  varnames[i]
     cb.name <-   paste("## `", varnames[i], "`  \n\n")
     cb.label <-  paste("> **Label:  **", object@description[[idx]], "  ")
-    cb.type <-   paste("> **Type:   **", ifelse(varnames[i]=="",  "data.frame", 
+    cb.type <-   paste("> **Type:   **", ifelse(varnames[i]=="DATA",  "data.frame", 
                                                 object@summary$classes[[idx]]), "  ")
     cb.source <- paste("> **Source Name: **", object@sources[[idx]]$Name, "  ")
     cb.source <- paste("> **Source Year: **", object@sources[[idx]]$Year, "  ")
